98c635d0b033b0f9ca3a63a3b90aa4819794d46e
amcmullen <amcmullen9291@gmail.com>
2022-01-14 09:35:24 -0500

    react-router-dom and skeleton framework

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Welcome from './Components/Welcome';
+import Page1 from './Components/Page1';
 
+// add email form for appointments?
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+          <Route path="/" element={<Welcome/>} />
+          <Route path="/breeded-shelter/main" element={<Menu />} />
+          <Route path="/breeded-shelter/Staff" element={<Page1 />} />
+        </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/Page1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Page1(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Page1;
