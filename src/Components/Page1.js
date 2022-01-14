d70b1bdad2b3e846543f67568745445dd5cb7fb2
amcmullen <amcmullen9291@gmail.com>
2022-01-14 13:12:58 -0500

    smooth shading transition with text insert

--- src/Components/Page1.js
+++ src/Components/Page1.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
 function Page1(props) {
+
+    function removeText(e){
+        e.preventDefault();
+        document.getElementById("transitions").innerHTML = " ";
+    }
+
+    function putTextBack(e){
+        e.preventDefault();
+        setTimeout(() => {
+            document.getElementById("transitions").innerHTML = "Hover over me!";
+        }, 2000);
+    }
+
+
     return (
         <>
         <div id="container">
-            <div id="placeholder"></div>
+            <div id="transitions" onMouseEnter={(e) => {removeText(e)}} onMouseLeave={(e) => {putTextBack(e)}}><center>Hover over me!</center></div>
         </div>
         <div id="navbar">
         <a href={"/"}><button>Home</button></a><br/>
--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -4,14 +4,14 @@ function Welcome(props) {
     return (
         <>
         <div id="container">
-            <div id="box"></div>
+                <div id="box"></div>
+                <div id="navbar">
+                    <a href={"/page1"}><button>Page1</button></a><br/>
+                    <button>Page2</button><br/>
+                    <button>Page3</button><br/>
+                    <button>Page4</button><br/>
+                </div>
         </div>
-        <div id="navbar">
-        <a href={"/page1"}><button>Page1</button></a><br/>
-        <button>Page2</button><br/>
-        <button>Page3</button><br/>
-        <button>Page4</button><br/>
-      </div>
       </>
     );
 }
