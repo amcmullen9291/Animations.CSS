918db2be3f452210f3aa8d61e5546f35f72f3a3d
amcmullen <amcmullen9291@gmail.com>
2022-01-15 13:49:36 -0500

    wavy effect with color and delay variables

--- src/Components/Page2.js
+++ src/Components/Page2.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import './AdditionalCSS/MoreStyling.css';
 
 function Page2(props) {
     return (
         <>
+        <div id="hoppingtext">
+          <div className="hop">
+              <span>H</span>
+              <span>E</span>
+              <span>L</span>
+              <span>L</span>
+              <span>O</span>
+              <span>.</span>
+          </div>
+        </div>
+        <div id="wavytext">
+          <div className="wavy">
+              <span>H</span>
+              <span>E</span>
+              <span>L</span>
+              <span>L</span>
+              <span>O</span>
+              <span>.</span>
+              <span>.</span>
+              <span>.</span>
+          </div>
+        </div>
         <div id="navbar">
         <a href={"/Page1"}><button>Page1</button></a><br/>
         <a href={"/"}><button>Home</button></a><br/>
--- src/Components/TestArea.js
+++ src/Components/TestArea.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './AdditionalCSS/MoreStyling.css';
 
 function TestArea(props) {
     return (
@@ -9,6 +10,17 @@ function TestArea(props) {
         </div>
         <br/>
         <div id="oneSideGradient">"A One sided conic gradient border"</div>
+        <hr/>
+        <div id="hoppingtext">
+            <div className="hop">
+                <span>H</span>
+                <span>E</span>
+                <span>L</span>
+                <span>L</span>
+                <span>O</span>
+                <span>.</span>
+            </div>
+        </div>
         </>        
     );
 }
