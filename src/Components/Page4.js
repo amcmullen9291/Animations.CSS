7fcd59da1017dee5bc30dd5783389581efcbe1ab
amcmullen <amcmullen9291@gmail.com>
2022-01-16 07:15:05 -0500

    page4 animations

--- src/Components/Page4.js
+++ src/Components/Page4.js
@@ -3,6 +3,64 @@ import React from 'react';
 function Page4(props) {
     return (
         <>
+<div id="Text3DArea">
+    <div className="TextBanner">
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    <div className="TextPanel"></div>
+    </div>
+    <div id="Text3DArea">
+    <div className="TextBanner">
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    <div className="TextPanel" id="secondText"></div>
+    </div>
+    </div>
+    </div>
         <div id="navbar">
             <a href={"/Page1"}><button>Page1</button></a><br/>
             <a href={"/Page2"}><button>Page2</button></a><br/>
